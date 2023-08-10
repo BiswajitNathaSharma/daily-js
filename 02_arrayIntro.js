1159e707928c90302a30b314ab3a4a659f576911
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2023-08-10 09:49:37 +0530

    introduction to array

---
+++ 02_arrayIntro.js
@@ -0,0 +1,14 @@
+// array is a data type on javaScript that can hold all type of values on it.
+//ex- int,char,string, object, boolan etc.
+
+
+// simple declaration of array syntax is bellow.
+const iAmArray = ["sharma",'p', 34, true, {name : 'biswajit', age: 20}]
+
+
+
+// array can it self contain another arrays
+const ArrayWithArray = [
+    ['sharma', 20],
+    ['biswajit', true]
+]
