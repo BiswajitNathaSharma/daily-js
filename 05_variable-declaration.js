ae4b6c0f2c14090ee7dd9944671ffd8e0cfeb5b0
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2023-08-11 20:33:25 +0530

    introduction to declaration of a variable

---
+++ 05_variable-declaration.js
@@ -0,0 +1,12 @@
+const accountId = 123499
+var userEmail = "example@mail.com"
+let userAge = 28
+
+//accountId= 33030 //returns  error because you cant modify a variable if you declared by const keyword
+
+/*prefer not to use var keyword to declare a variable.
+because it returns problems on on scope and block concept  */
+
+console.table([accountId, userEmail, userAge]) //it returns the output on a tabular format.
+
+
