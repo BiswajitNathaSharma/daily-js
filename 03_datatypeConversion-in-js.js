32fde1b4c1d01f80e61c84b5f1111e287e73958d
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2023-08-14 17:41:40 +0530

    change num and bool datatypes to other data types

---
+++ 03_datatypeConversion-in-js.js
@@ -0,0 +1,63 @@
+// investigational study about numbers in javaScript. 
+
+
+let score = "89aaa"
+console.log(typeof(score)); // string
+
+let scoreToNumber = Number(score)
+console.log(typeof(scoreToNumber)); //number
+
+console.log(scoreToNumber);     //NaN- not a number.
+
+/* 
+let temprature = ""
+let tempratureToNumber = Number(temprature)
+console.log(tempratureToNumber);        //0 
+ */
+
+/*
+let temprature = true
+let tempratureToNumber = Number(temprature)
+console.log(tempratureToNumber);        //1
+*/
+
+/*
+let temprature = false
+let tempratureToNumber = Number(temprature)
+console.log(tempratureToNumber);        //0
+*/
+/*
+let temprature = undefined
+let tempratureToNumber = Number(temprature)
+console.log(tempratureToNumber);        //NaN
+*/
+
+
+
+/*  
++++++++++++++++++++++++++++++NOTES+++++++++++++++++++++++++++++
+"33"  -> 33
+true ->  1
+false -> 0
+undefined -> NaN
+"sharma"    -> NaN
+""      -> 0
+ */
+
+// ++++++++++++++++++++++++++++++++++++++BOOLEAN+++++++++++++++++++++++++++++
+
+let isLogedIn = 0
+let ToBoolean = Boolean(isLogedIn)
+console.log(ToBoolean);
+/*  
++++++++++++++++++++++++++++++NOTES+++++++++++++++++++++++++++++
+0/-0   -> false
+any integer ->true
+"33"  -> true
+true ->  true
+false -> false
+undefined -> false
+"sharma"    -> true
+""      -> false
+ */
+
