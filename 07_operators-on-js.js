666819da1881cfc67d7bd5c957f5efad7b12fb1c
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2023-08-16 08:39:47 +0530

    assignment n arithmatic operator in js

---
+++ 07_operators-on-js.js
@@ -0,0 +1,32 @@
+// ASSIGNMENT OPERATOR
+let a = 1; //assignment
+
+let b = 2;
+b += 3;     //addition assignment   b = b +3
+
+let c = 2;
+c -= 3;     //substraction assignment   c = c - 3
+
+let d = 2;
+d *= 3;     //multiplication assignment d = d * 3
+
+let e = 2;
+(e /= 3);     //devision assignment       c = c / 3
+
+let f = 2;
+f %= 3;     //modulous assignment       c = c % 3
+
+
+// console.log(a, b, c, d, Math.round(e), f);
+
+
+
+// arithmatic operator
+a % b //reminder operatror: returns the reminder of the devision of given two numbers
+
+a++;    // post increment operator: increments the value by one (first print the value and then increment it).
+++a;    // pre increment operator: increments the value by one (first increment the value and then print it).
+// same concept apply on decrement also.
+
+// console.log(a ** b); //exponantional operator: returns 3^5 = 243
+
