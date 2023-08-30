096caa5ffb730d6727b12c8b0342c6b3cff3ba78
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2023-08-30 12:02:57 +0530

    scope introduction

---
+++ 18_scope-intro.js
@@ -0,0 +1,23 @@
+// a curly-bracket denotes the scope. if a variable is declared inside the scope then it should not be used outside the scope, if it can be accessed out side the scope then it is can create a bad situation.
+// thats why peoples in today dont use var any more, because it can be access outside the scope: follow the following code snipped. 
+
+if (true) {
+    let a = 2
+    const b = 3
+    var c = 4
+}
+
+// console.log(a);  //error
+// console.log(b);  //error
+// console.log(c);     // 3
+
+let a = 5
+if (true) {
+    let a = 2
+    const b = 3
+    console.log(a);     /* 2: Although it is inside the the if scope it returns the
+                            value thet assign to it inside the scope. */
+}
+
+console.log(a);         /* 5: because it is declared out side the if scope it cant 
+                            access the value inside the if scope. */
