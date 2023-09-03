5dfee5f4f014b7dfd0dd6c285dcd9abf7f96cb78
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2023-09-03 17:25:21 +0530

    switch statement learned

---
+++ 21_file/23_switch-statement.js
@@ -0,0 +1,40 @@
+//SWITCH CASES USING number DATATYPE
+
+let month = 3
+switch (month) {
+    case 1:
+        console.log('jan');
+        break;
+    case 2:
+        console.log('feb');
+        break;
+    case 3:
+        console.log('march');
+        break;
+    case 4:
+        console.log('apr');
+        break;
+
+    default:console.log('select a valid month');
+}
+
+
+//SWITCH CASES USING string DATATYPE
+
+let months = "mar"
+switch (months) {
+    case "jan":
+        console.log('jan');
+        break;
+    case "feb":
+        console.log('feb');
+        break;
+    case "mar":
+        console.log('march');
+        break;
+    case "apr":
+        console.log('apr');
+        break;
+
+    default:console.log('select a valid month');
+}
