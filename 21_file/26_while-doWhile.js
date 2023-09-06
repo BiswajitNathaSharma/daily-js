e0c31f629f20e64d253ba28c42e10e1c70101673
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2023-09-06 21:30:34 +0530

    while, do-while done

---
+++ 21_file/26_while-doWhile.js
@@ -0,0 +1,14 @@
+// while loop on js: while loop first checks the condition if true, then executes the code inside it.
+
+let i = 1
+while (i <= 10) {
+    console.log(i);
+    i++
+}
+
+// do-while loop on js: do-while loop first executes the code inside then checks the condition, if true then code will execute again. special thing of its is it will run the code once even the condition is false.
+let j = 1
+do {
+    console.log(j);
+    j++
+} while (j <= 10);
