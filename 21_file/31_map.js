b801549cbe0499ab823c896b185215ba90573f5c
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2023-09-26 08:54:34 +0530

    usw foreach insted of map for better understanding

--- 21_file/31_map.js
+++ 21_file/31_map.js
@@ -3,3 +3,10 @@ const newNums = myNums.map((num)=>{
     return num+10
 })
 console.log(newNums);
+
+// Use foreach here to replace map function
+let myNewNums = []
+myNums.forEach((num)=>{
+    myNewNums.push(num+10)
+})
+console.log(myNewNums);
