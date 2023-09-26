fee1cfc2f1538e12721c692de8f2746709d810d4
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2023-09-26 08:55:07 +0530

    learn chaining in function

--- 21_file/32_chainning.js
+++ 21_file/32_chainning.js
@@ -1,3 +1,3 @@
 let myNums = [1, 2, 3, 4, 5, 6, 7]
-const newNums = myNums.map((num)=> num+10).map((num)=> num+2).filter((num)=>num>14)
+const newNums = myNums.map((num)=> num+10).map((num)=> num+2).filter((num)=>num>14) 
 console.log(newNums);
